cda9f9913297c09d19f9d7c15552a05e7de48b02
Thanos Makatos <thanos.makatos@citrix.com>
2014-06-10 09:48:31 +0100

    CA-136142: increase maximum number of blktap devices to 2048
    Imported using git-import-from-patch
    From repo linux-3.x.pg rev 5bbfe1dd2f9e6625627b33f2b9da8e0a317e301c
    Patch: master/blktap2-import-from-trunk.patch
    Patch: master/blktap2-fixups.patch

--- drivers/block/blktap2/blktap.h
+++ drivers/block/blktap2/blktap.h
@@ -24,8 +24,7 @@ extern int blktap_device_major;
 #define BTWARN(_f, _a...)            BTPRINTK(0, KERN_WARNING, 0, _f, ##_a)
 #define BTERR(_f, _a...)             BTPRINTK(0, KERN_ERR, 0, _f, ##_a)
 
-#define MAX_BLKTAP_DEVICE            1024
-
+#define MAX_BLKTAP_DEVICE            2048
 #define BLKTAP_DEVICE                4
 #define BLKTAP_DEVICE_CLOSED         5
 #define BLKTAP_SHUTDOWN_REQUESTED    8
